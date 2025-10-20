d9cbf51ef870f81bc65a73c3392b2fb079990ccd
STQT <productgsm@gmail.com>
2025-10-21 01:03:14 +0500

    adding favicon and robots files

--- axmedova_project/urls.py
+++ axmedova_project/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
 from core.sitemaps import StaticViewSitemap, BlogPostSitemap, PublicationSitemap, BookSitemap
 
 sitemaps = {
@@ -20,6 +21,7 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('core.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots'),
 ]
 
 # Serve media files in development and production (fallback)
